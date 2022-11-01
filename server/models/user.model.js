86c4eb881884d2a24a9233e661ea8ba4c54578ce
Charan Karthik <secharank@gmail.com>
2022-11-01 14:30:14 -0700

    user model update

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true, minlength: 8},
 })
