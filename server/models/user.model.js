93c6c428a2ac7a12df1c3ebf1c0545bde4ffd88d
Charan Karthik <secharank@gmail.com>
2022-11-01 16:01:57 -0700

    server side updates

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true, minlength: 8},
-})
+    username: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+    },
+    password: { type: String, required: true, minlength: 8 },
+}, { timestamps: true })
+
+UserSchema.virtual('confirmPassword')
+    .get(() => this._confirmPassword)
+    .set(value => this._confirmPassword = value);
+
+UserSchema.pre('validate', function (next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password');
+    }
+    next();
+});
+
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
 
 const User = mongoose.model('User', UserSchema)
-module.exports(User)
+module.exports = User
